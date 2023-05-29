1207531729d0663d7139d996c12ed0346bbde9c5
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2023-05-29 21:48:30 +0200

    todo

--- main.cpp
+++ main.cpp
@@ -1,3 +1,36 @@
+/*
+ * TODO:
+ * Warto jeszcze dodać rysunek klas z powiązaniami... Proszę przemyśleć pomysł,
+ *  - czy nie dało by się utworzyć klasy waluta (abstrakcyjna) i z niej wyprowadzić konkretne waluty zapisane w tablicy,
+ *  - Zmiana wartosci waluty co wlaczenie programu
+ *      - generowanie tych wartosci w jakims danym zakresie
+ *  - znalezc jakiego przyklady UI
+ *
+ *  - PANEL USER:
+ *      - main panel
+ *          - wplacanie srodkow
+ *
+ *          - wyplacanie srodkow
+ *
+ *          - zmiana hasla
+ *
+ *          - usuniecie konta
+ *
+ *          - podglad na aktualne ceny walut
+ *
+ *          - wyloguj
+ *
+ *  - PANEL ADMIN:
+ *      - main panel
+ *          - zazadzanie userem
+ *              - usuwanie
+ *              - zmiana salda
+ *              - dodawanie nowych kont
+ *
+ * DONE:
+ *   - Drobna uwaga: jak ID nadawane jest na stale to może z tego zrobić const? DONE
+ * */
+
 #include "main.h"
 
 int main(){
